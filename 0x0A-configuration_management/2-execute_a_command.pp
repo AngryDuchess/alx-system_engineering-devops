ba883a2061644730702b5740f9ea31085bcea948
Hamida Mahama <mahamahamida@gmail.com>
2023-03-28 08:05:03 +0000

    fixed

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 #This file creates a manifest that kills a process named 'killmenow'
 
-exec { 'killmenow'
+exec { 'killmenow':
   command   => '/usr/bin/pkill -f killmenow',
   onlyif    => '/usr/bin/pgrep -f killmenow',
   path      => '/usr/bin',
