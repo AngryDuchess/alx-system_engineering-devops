a012a7ee7e0ac25f890901b9193bf504e3063ba8
Hamida Mahama <mahamahamida@gmail.com>
2023-03-28 07:49:07 +0000

    comments

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-file { '/tmp/school':
-  ensure => file,
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
-  content => 'I love Puppet',
+file { '/tmp/school':  # The resource name is file
+  ensure => file,  # the ensure parameter is set to file
+  mode => '0744',  # mode is set to specified value
+  owner => 'www-data',  # owner is set to specified value
+  group => 'www-data',  # group is set to specified value
+  content => 'I love Puppet',  # content is set to string 'i love you'
 }
