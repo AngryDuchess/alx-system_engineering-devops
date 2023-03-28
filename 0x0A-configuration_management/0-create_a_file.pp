801c92f255580a2ed892aaa64b8d1b995f0c0f2f
Hamida Mahama <mahamahamida@gmail.com>
2023-03-28 07:57:16 +0000

    comments

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# This script creates a file in /tmp with File permission,owner, group and content specified
+# This script creates a file in /tmp
+
 file { '/tmp/school':
-  ensure => file,
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
