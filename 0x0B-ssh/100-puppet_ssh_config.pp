e1acab5b55e6b7de5fe07b463660613d59767e8b
Hamida Mahama <mahamahamida@gmail.com>
2023-04-27 14:27:03 +0000

    puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# Configuring ssh config file with puppet
+include stdlib
+
+file_line { 'Turn off passwd auth':
+	ensure	=> present,
+	path 	=> 'etc/ssh/ssh_config'
+	line 	=> '	PasswordAuthentication no',
+	replace => true,
+}
+
+file_line {	'Declare identity file':
+	ensure	=> present,
+	path	=> 'etc/ssh/ssh_config',
+	line	=> '	identityFile ~/.ssh/school',
+	replace => true,
+}
