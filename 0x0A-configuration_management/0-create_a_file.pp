ac87482705038049908eafac0208a211786db82a
Mahama Hamida <mahamahamida@gmail.com>
2023-11-26 21:05:17 +0000

    fixed some style errors

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
+# Creates a file school in a specified path and adds permisions
 file { 'school':
-  path   => '/tmp/school',
+  path    => '/tmp/school',
   content => 'I love Puppet',
-  group  => 'www-data',
-  owner  => 'www-data',
-  mode   => '0744'
+  group   => 'www-data',
+  owner   => 'www-data',
+  mode    => '0744'
 }
