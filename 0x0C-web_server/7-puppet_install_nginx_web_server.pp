0879953c7ae12ecaa5744423d701154568fef7c9
Mahama Hamida <mahamahamida@gmail.com>
2023-12-05 00:41:52 +0000

    7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,30 +1,32 @@
 # nginx configuration using puppet
 
-exec { 'apt-get update':
-    command => 'apt-get update',
-    path    => ['/usr/bin', '/usr/sbin']
+exec { 'apt_update':
+  command => 'apt-get update',
+  path    => ['/usr/bin', '/usr/sbin'],
 }
 
 package { 'nginx':
-    ensure => installed
+  ensure  => installed,
+  require => Exec['apt_update'],
 }
 
-file { '/var/www/html/index.nginx-debian.html':
-    ensure  => file,
-    content => 'Hello World!',
-    mode    => '0664',
-    require => Package['nginx']
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+  mode    => '0664',
+  require => Package['nginx'],
 }
 
-$line = '\n\tlocation /redirect_me {\n\t\treturn 301 https://www.hamidas-portfolio.webflow.io;\n\t}\n'
-
-exec { 'redirection':
-    command => "sudo sed -i 'server_name _;/a ${line}' /etc/nginx/sites-available/default",
-    path    => '/usr/bin:/bin/'
+$redirect_rule = '\n\tlocation /redirect_me {\n\
+        \treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\
+        }'
+exec { 'redirect_me':
+  command => "sudo sed -i '/server_name _;/a \\ ${redirect_rule}' /etc/nginx/sites-available/default",
+  path    => '/usr/bin:/bin',
 }
 
 service { 'nginx':
-    ensure  => running,
-    enable  => true,
-    require => [Package['nginx'], Exec['redirection']]
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
 }
