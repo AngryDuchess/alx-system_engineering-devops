489ff77ff8f45453338b37eb600686f2fd7500bb
Mahama Hamida <mahamahamida@gmail.com>
2023-12-06 01:39:07 +0000

    puppet config

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,1 +1,15 @@
-# A puppet file
+# A puppet file to configure a new ubuntu machine with a custom http header response
+
+$CONFIG_FILE="/etc/nginx/sites-available/default"
+$HEADER="\n\tadd_header X-Served-By \$hostname;"
+
+exec { 'update and install nginx':
+    command  => 'sudo apt-get update -y && sudo apt-get install nginx -y',
+    provider => shell,
+}
+
+exec { 'insert header':
+    command  => 'sudo sed -i "/server_name _;/a\ $HEADER" "$CONFIG_FILE"' && sudo service nginx restart',
+    provider => shell
+    require  => Exec['update and install nginx']
+}
