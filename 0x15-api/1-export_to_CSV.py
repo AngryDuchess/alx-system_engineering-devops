b8810bb921f469b08cf4f518a3ffde69c81c5987
Mahama Hamida <mahamahamida@gmail.com>
2024-01-05 15:34:10 +0000

    add: new task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,26 +1,35 @@
 #!/usr/bin/python3
-"""stores data as csv"""
-import csv
-import requests
+""" Requests and displays todos of a user """
+from requests import get
 from sys import argv
+from csv import writer, QUOTE_ALL
 
+if __name__ == "__main__":
+    user_id = int(argv[1])
 
-if __name__ == '__main__':
+    def get_user(id):
+        """ Gets th username """
+        res = get(f'https://jsonplaceholder.typicode.com/users/{id}')
+        return res.json().get('username')
 
-    userID = int(argv[1])
-
-    username = requests.get(
-        f'https://jsonplaceholder.typicode.com/users/{userID}'
-        ).json().get('username')
-    todos = requests.get(f'https://jsonplaceholder.typicode.com/todos/').json()
-    user_todos = list(filter(lambda todo: todo.get('userId') == userID, todos))
-
-    to_csv = list(map(lambda todo: [str(userID), username,
-                  str(todo.get('completed')), todo.get('title')], user_todos))
+    def get_todo(id):
+        """ Returns data from todo response """
+        res = get('https://jsonplaceholder.typicode.com/todos')
+        user_todos = list(filter(lambda todo: todo.get('userId') == id,
+                                 res.json()))
+        name = get_user(user_id)
+        return list(map(lambda item: [
+            str(user_id),
+            name,
+            str(item.get('completed')),
+            item.get('title')
+        ], user_todos))
 
     try:
-        with open(f'{userID}.csv', 'w') as file:
-            csv_file = csv.writer(file, quoting=csv.QUOTE_ALL)
-            csv_file.writerows(to_csv)
+        with open(f'{user_id}.csv', 'w', newline='') as file:
+            csv_file = writer(file, quoting=QUOTE_ALL)
+            csv_file.writerows(get_todo(user_id))
     except Exception:
         pass
+
+    get_todo(user_id)
