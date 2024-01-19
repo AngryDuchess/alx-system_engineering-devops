ae9eff10b1f0f88cbb3cbe76405e165125abc095
Mahama Hamida <mahamahamida@gmail.com>
2024-01-19 23:43:49 +0000

    Refactor number_of_subscribers function to handle empty data response

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,7 +14,11 @@ def number_of_subscribers(subreddit):
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        return response.json().get('data').get('subscribers')
+    if response.ok:
+        data = response.json().get('data').get('subscribers')
+        if data:
+            return int(data)
+        else:
+            return 0
     else:
         return 0
