52bc08a825ec97c6f39a052a2b01103a1f2fdcd1
Mahama Hamida <mahamahamida@gmail.com>
2024-01-19 23:47:54 +0000

    Refactor number_of_subscribers function to use requests.get and improve code readability

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
-"""
-queries the Reddit API and returns the number of subscribers
-for a given subreddit
-"""
-import requests
+""" Queries and return the number of subscribers in sub reddit """
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """
-    gets the number of subscribers for the
-    subreddit passed to the function
-    """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
-    if response.ok:
-        data = response.json().get('data').get('subscribers')
-        if data:
-            return int(data)
+    """ Queries the reddit API"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    res = get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    if res.ok:
+        data = res.json()
+        if data.get('data').get('subscribers'):
+            return int(data['data']['subscribers'])
         else:
             return 0
     else:
