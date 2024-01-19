c0ee998e511ecbccff9db8e1141a669e8f8f5ce5
Mahama Hamida <mahamahamida@gmail.com>
2024-01-19 23:08:24 +0000

    add: recursive script

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,7 +9,7 @@ import requests
 def recurse(subreddit, hot_list=[], after=None, count=0):
     """
     function that queries the Reddit API and returns a list containing
-    the titles of all hot articles for a given subreddit. 
+    the titles of all hot articles for a given subreddit.
     """
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     if after:
@@ -25,5 +25,4 @@ def recurse(subreddit, hot_list=[], after=None, count=0):
         hot_list.append(children.get('data').get('title'))
     if after:
         return recurse(subreddit, hot_list, after, count)
-    else:
-        return hot_list
+    return hot_list
