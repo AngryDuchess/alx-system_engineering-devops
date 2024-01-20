64fcf9a9300f0e650c05262cb80950ff49a88fb8
Mahama Hamida <mahamahamida@gmail.com>
2024-01-20 23:33:13 +0000

    add: readme and script

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# debugging a wordpress website
+
+exec {'fixing server error':
+    command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+    path    => '/bin'
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,2 @@
+# Web stack debugging #3
+This project introduces the student to use strace to debug a faulty wordpress website
